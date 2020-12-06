e0c7dca4c3c8ca5c12aebf42ea1d0cc37bfb1d85
bogdankvlv <70582969+bogdankvlv@users.noreply.github.com>
2020-12-05 22:26:53 -0500

    Updates

---
+++ precache-manifest.52ece7c35b7c02fd70667c11dca0e81a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34733ddab9fcb754a6835b6e187e0ef6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7f5d3411629deeae6b77",
+    "url": "/portfolio/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "5db600c507c765d2a412",
+    "url": "/portfolio/static/css/main.1b917edd.chunk.css"
+  },
+  {
+    "revision": "7f5d3411629deeae6b77",
+    "url": "/portfolio/static/js/2.7a74a2a5.chunk.js"
+  },
+  {
+    "revision": "5db600c507c765d2a412",
+    "url": "/portfolio/static/js/main.cfcfdc8e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ac2b21753bc4a4c01d97ee56796a9d43",
+    "url": "/portfolio/static/media/bodya.ac2b2175.jpg"
+  }
+]);
